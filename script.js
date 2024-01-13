e4769d924fe407b2a6b97f0d152a9db6b52f97c9
Akash Gogoi <gogoiakash09@gmail.com>
2024-01-14 00:54:07 +0530

    add logic for fetching data using lat and lon

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-
-async function fetchData() {
+async function fetchGeocode() {
   const res = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=khumtai&appid=26bb0823f324a9fb0d23f5373a61429a&units=metric"
+    "https://maps.googleapis.com/maps/api/geocode/json?address=melamora&key=AIzaSyBkT8Xulai_tu3DEIaX-xWYXkvsHEyum-4"
   );
 
   const data = await res.json();
-     console.log(Math.round(data.main.temp))
+
+  const latitude = data.results[0].geometry.location.lat;
+  const longitude = data.results[0].geometry.location.lng;
+
+  return { latitude, longitude }
+}
+
+async function fetchTempData(latitude, longitude) {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=26bb0823f324a9fb0d23f5373a61429a&units=metric`
+  );
+
+  const data = await res.json();
   return data;
 }
 
-fetchData();
-
-async function updateData() {
-  const currData = document.getElementById("temp-data")
+async function logData() {
   try {
-    const data = await fetchData();
-    currData.textContent = data.main.temp;
+    const { latitude, longitude } = await fetchGeocode();
+    const weatherData = await fetchTempData(latitude, longitude);
+    console.log(Math.round(weatherData.main.temp));
   } catch (error) {
-    console.log(error);
+    console.error("Error:", error);
   }
 }
 
-updateData();
+logData();
+
+// async function updateData() {
+//   const currData = document.getElementById("temp-data")
+//   try {
+//     const data = await fetchData();
+//     // currData.textContent = data.main.temp;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// updateData();
